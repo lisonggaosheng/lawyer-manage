e160f0d5f43a14f2194457e5c94660e3536c3e0f
guozhipeng <guozhipeng@dadimedia.com>
2018-04-09 17:30:45 +0800

    jsp方式修改

--- src/main/java/com/lawyer/config/shiro/ShiroConfig.java
+++ src/main/java/com/lawyer/config/shiro/ShiroConfig.java
@@ -17,12 +17,50 @@ import org.springframework.web.filter.DelegatingFilterProxy;
 import com.google.common.collect.Maps;
 import com.lawyer.config.redis.MyRedisProperties;
 
-import lombok.extern.slf4j.Slf4j;
-
-//@Configuration
-@Slf4j
+@Configuration
 public class ShiroConfig {
 
+	@Bean
+	public FilterRegistrationBean delegatingFilterProxy(){
+	    FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+	    DelegatingFilterProxy proxy = new DelegatingFilterProxy();
+	    proxy.setTargetFilterLifecycle(true);
+	    proxy.setTargetBeanName("shiroFilter");
+	    filterRegistrationBean.setFilter(proxy);
+	    return filterRegistrationBean;
+	}
+	/**
+	 * @see org.apache.shiro.spring.web.ShiroFilterFactoryBean
+	 * @return
+	 */
+	@Autowired
+	@Bean(name = "shiroFilter")
+	public ShiroFilterFactoryBean shiroFilter() {
+		ShiroFilterFactoryBean shiroFilterFactoryBean  = new ShiroFilterFactoryBean();
+		shiroFilterFactoryBean.setSecurityManager(securityManager());
+		// 如果不设置默认会自动寻找Web工程根目录下的"/login.jsp"页面
+        shiroFilterFactoryBean.setLoginUrl("/toLogin");
+        // 登录成功后要跳转的链接
+        shiroFilterFactoryBean.setSuccessUrl("/index");
+        //未授权界面;
+        shiroFilterFactoryBean.setUnauthorizedUrl("/unauthor");
+
+		Map<String, String> filterChainDefinitionMap  = Maps.newHashMap();
+		// 配置不会被拦截的链接 顺序判断
+        filterChainDefinitionMap.put("/static/**", "anon");
+        filterChainDefinitionMap.put("/templates/**", "anon");
+        filterChainDefinitionMap.put("/toLogin", "anon");
+        filterChainDefinitionMap.put("/login", "anon");
+        //配置退出 过滤器,其中的具体的退出代码Shiro已经替我们实现了
+        filterChainDefinitionMap.put("/logout", "logout");
+        // <!-- 过滤链定义，从上向下顺序执行，一般将 /**放在最为下边 -->:这是一个坑呢，一不小心代码就不好使了;
+        // <!-- authc:所有url都必须认证通过才可以访问; anon:所有url都都可以匿名访问-->
+        filterChainDefinitionMap.put("/**", "authc");
+        
+		
+		shiroFilterFactoryBean.setFilterChainDefinitionMap(filterChainDefinitionMap);
+		return shiroFilterFactoryBean;
+	}
 	/**
 	 * 加载属性文件数据
 	 * 
@@ -48,40 +86,7 @@ public class ShiroConfig {
 		return filterRegistration;
 	}
 
-	/**
-	 * @see org.apache.shiro.spring.web.ShiroFilterFactoryBean
-	 * @return
-	 */
-	@Autowired
-	@Bean(name = "shiroFilter")
-	public ShiroFilterFactoryBean shiroFilter() {
-		ShiroFilterFactoryBean shiroFilterFactoryBean  = new ShiroFilterFactoryBean();
-		shiroFilterFactoryBean.setSecurityManager(securityManager());
-		// 如果不设置默认会自动寻找Web工程根目录下的"/login.jsp"页面
-        shiroFilterFactoryBean.setLoginUrl("/toLogin");
-//		 shiroFilterFactoryBean.setLoginUrl("/index");
-        // 登录成功后要跳转的链接
-        shiroFilterFactoryBean.setSuccessUrl("/index");
-      //未授权界面;
-        shiroFilterFactoryBean.setUnauthorizedUrl("/unauthor");
-
-		Map<String, String> filterChainDefinitionMap  = Maps.newHashMap();
-		// 配置不会被拦截的链接 顺序判断
-        filterChainDefinitionMap.put("/static/**", "anon");
-        filterChainDefinitionMap.put("/templates/**", "anon");
-        filterChainDefinitionMap.put("/toLogin", "anon");
-        filterChainDefinitionMap.put("/login", "anon");
-        //配置退出 过滤器,其中的具体的退出代码Shiro已经替我们实现了
-        filterChainDefinitionMap.put("/logout", "logout");
-        // <!-- 过滤链定义，从上向下顺序执行，一般将 /**放在最为下边 -->:这是一个坑呢，一不小心代码就不好使了;
-        // <!-- authc:所有url都必须认证通过才可以访问; anon:所有url都都可以匿名访问-->
-//        filterChainDefinitionMap.put("/**", "authc");
-        
-		
-		shiroFilterFactoryBean.setFilterChainDefinitionMap(filterChainDefinitionMap);
-		return shiroFilterFactoryBean;
-	}
-
+	
 	/**
 	 * 权限管理器
 	 * 
@@ -115,10 +120,8 @@ public class ShiroConfig {
 	 * @return
 	 */
 	@Bean
-//	@DependsOn(value = { "lifecycleBeanPostProcessor", "shrioRedisCacheManager" })
 	public UserRealm userRealm() {
 		UserRealm userRealm = new UserRealm();
-//		userRealm.setCacheManager(redisCacheManager());
 		userRealm.setCachingEnabled(true);
 		userRealm.setAuthenticationCachingEnabled(false);//禁用认证缓存
 		userRealm.setAuthorizationCachingEnabled(true);
